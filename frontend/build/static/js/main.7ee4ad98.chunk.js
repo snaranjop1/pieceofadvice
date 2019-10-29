2998555d6848b53b9034184f97b292e5e5b5f5ba
snaranjop1 <s.naranjop1@uniandes.edu.co>
2019-10-28 20:53:30 -0500

    Initial proyect structure

---
+++ frontend/build/static/js/main.7ee4ad98.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpfrontend=this.webpackJsonpfrontend||[]).push([[0],{3:function(e,n,t){e.exports=t(8)},8:function(e,n,t){"use strict";t.r(n);var r=t(0),o=t.n(r),c=t(2);var a=function(){return o.a.createElement("div",null,o.a.createElement("h1",null,"Welcome to piece of advice"))};t.n(c).a.render(o.a.createElement(a,null),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.7ee4ad98.chunk.js.map
