fe049efd8eef97d3158af4a662298ea9c54420c5
snaranjop1 <s.naranjop1@uniandes.edu.co>
2019-11-03 06:55:22 -0500

    App fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import AdvicePage from "./AdvicePage";
 import MainPage from "./MainPage";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
+  let default_info = [
+    {
+      id: "000",
+      err: "err",
+      question: "",
+      detail: "",
+      items: []
+    }
+  ];
+  let [advices, setAdvices] = useState(default_info);
+
+  useEffect(() => {
+    fetch("advice-rooms")
+      .then(res => res.json())
+      .then(data => {
+        setAdvices(data);
+        console.log("advices", advices);
+      });
+  }, []);
+
   return (
     <Router>
       <div id="content-container">
@@ -11,15 +31,7 @@ function App() {
           <Route path="/" exact component={MainPage} />
           <Route
             path="/advice"
-            component={() => (
-              <AdvicePage
-                props={{
-                  id: "AF78D9",
-                  question: "How should I continue?",
-                  detail: "like seriously"
-                }}
-              />
-            )}
+            component={() => <AdvicePage props={advices} />}
           />
         </Switch>
       </div>
